833459fd67167bd425c2706e000ca0d98c1889a9
Michael Platzer <michael.platzer@gmail.com>
2024-12-07 15:00:45 +0100

    Version Bump to 1.3.2 (#44)

--- mostlyai/qa/__init__.py
+++ mostlyai/qa/__init__.py
@@ -21,7 +21,7 @@ from mostlyai.qa.report import report
 from mostlyai.qa.report_from_statistics import report_from_statistics
 
 __all__ = ["report", "report_from_statistics"]
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 if Version(pd.__version__) >= Version("2.2.0"):
